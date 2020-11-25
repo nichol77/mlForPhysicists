488b20b705bd4f66f4481869b4de08b7f1956a86
Ryan Nichol <r.nichol@ucl.ac.uk>
2020-11-25 21:31:00 +0000

    Adding a whooshier version of the Week7_RL notebook

--- Week7/Week7_RL.py
+++ Week7/Week7_RL.py
@@ -133,9 +133,49 @@ from matplotlib import gridspec
 from IPython.display import clear_output
 from time import sleep
 
+# +
+from enum import IntEnum
+class Dir(IntEnum):
+    Up=0
+    Down=1
+    Left=2
+    Right=3
 
-# -
 
+class GridWorld():
+    def __init__(self,nCol,nRow):
+        self.nCol=nCol
+        self.nRow=nRow
+        
+    def getZeroGrid(self):
+        return np.zeros((self.nRow,self.nCol))
+    
+    def getNumberGrid(self):
+        return np.arange(self.nRow*self.nCol).reshape(self.nRow,self.nCol)
+    
+    def getRandomPolicyGrid(self):
+        policy=np.ones((self.nRow,self.nCol,4))
+        return policy*0.25
+    
+    def go(self,row,col,way):
+        if way==Dir.Up:
+            row2=max(0,row-1)
+            col2=col
+        elif way==Dir.Down:
+            row2=min(self.nRow-1,row+1)
+            col2=col
+        elif way==Dir.Left:
+            row2=row
+            col2=max(0,col-1)
+        elif way==Dir.Right:
+            row2=row
+            col2=min(self.nCol-1,col+1)
+        return (row2,col2)
+    
+    def get(self,vk,row,col,way):
+        row2,col2=self.go(row,col,way)
+        return vk[row2,col2]
+            
 def plotGridWorld(cellText,showCorners, title=None, applyFormat=False):
     fig, ax = plt.subplots(figsize=(2,2))
     ax.axis('tight')
@@ -156,9 +196,10 @@ def plotGridWorld(cellText,showCorners, title=None, applyFormat=False):
                 cell.set_text_props(color='grey')
                 cell.set_color('grey') 
                 cell.set_edgecolor('black')        
-        cell._height = cell._width = 0.25
+        cell._height = cell._width = 1./nRow
     if title:
         ax.set_title(title)
+# -
 
 # ### Small Gridworld
 #
@@ -168,8 +209,8 @@ def plotGridWorld(cellText,showCorners, title=None, applyFormat=False):
 # - actions leading out of the grid leave the state unchanged (but still cost $-1$)
 #
 
-cellText=np.arange(16)
-cellText=cellText.reshape((4,4))
+gw=GridWorld(5,5)
+cellText=gw.getNumberGrid()
 plotGridWorld(cellText,title="The Small Gridworld",showCorners=False)
 
 # ### Random policy
@@ -178,17 +219,29 @@ plotGridWorld(cellText,title="The Small Gridworld",showCorners=False)
 # $$ v_{k+1}(s) = \sum_{a \in \mathcal{A} }0.25  \left( \mathcal{R}^a_s + \gamma v_k(s^\prime) \right) $$
 # where $s^\prime$ is the state thate we end up in following action $a$ from state $s$. So if we start in state 10 and following action up we end up in state 6.
 #
+# So let's start by initialising our grid world to have zero values everywhere.
 
-vk=np.zeros((4,4))
+vk=gw.getZeroGrid()
 plotGridWorld(vk,showCorners=True,title="Initial Value")
 
 
+# #### Updating our values
+#
+# Now we can use the update equation above to get a new estimate of the value. This new estimate will have two parts (for the non-terminal squares)
+# - -1 from moving
+# - the value of the square we move to
+#
+# We will use our random policy $\pi(a|s)=0.25$ to average over the available directions to move.
+
 # +
+
 #Unneccesarily verbose and loopy for clarity
-def updateVk(vk):
-    nRow=vl.shape[0]
-    nCol=vl.shape[1]
-    vl=np.zeros((nRow,nCol))
+#Unneccesarily verbose and loopy for clarity
+def updateVk(vk,policy):
+    global gw
+    nRow=vk.shape[0]
+    nCol=vk.shape[1]
+    vl=gw.getZeroGrid()
     for row in range(nRow):
         for col in range(nCol):
             #Check if we are in terminal state
@@ -197,72 +250,138 @@ def updateVk(vk):
             
             #Update our value based on the Bellman equation
             vl[row][col]=-1
-            vl[row][col]+=0.25*vk[row][min(nCol-1,col+1)] #Up
-            vl[row][col]+=0.25*vk[row][max(0,col-1)] #Down
-            vl[row][col]+=0.25*vk[max(0,row-1)][col] #Left
-            vl[row][col]+=0.25*vk[min(nRow-1,row+1)][col] #Right
+            for way in Dir:
+                vl[row][col]+=policy[row][col][way]*gw.get(vk,row,col,way)
     return vl
 
 #Reset the value to all zeros
-vk=np.zeros((4,4))
-vk=updateVk(vk) #Do first update
-plotGridWorld(vk,showCorners=True,title="1 step")
+vk=gw.getZeroGrid()
+rp=gw.getRandomPolicyGrid()
+vk=updateVk(vk,rp) #Do first updat
+plotGridWorld(vk,showCorners=True,title="Random-$\pi$ 1 step")
 
-plotHere=[2,3,4,5,10,20,40,80,120,160,200]
-for i in range(2,201):
-    vk=updateVk(vk)
+plotHere=[2,3,4,5,10,20,40,80,120,160,2000]
+for i in range(2,2001):
+    vk=updateVk(vk,rp)
     if i in plotHere:
-        plotGridWorld(vk,showCorners=True,title=str(i)+" steps",applyFormat=True)
+        plotGridWorld(vk,showCorners=True,title="Random-$\pi$ "+str(i)+" steps",applyFormat=True)
 
+# -
+
+# After ~160 steps the random walk policy has determined the optimal values of the squares. These values are essentially (-1 times) the average number of steps from that starting location to one of the goal states.
+#
+# ### Determining a better policy
+# We can also use our policy $\pi$ to determine a better policy by using the greedy algorithm. The greedy algorithm will favour the directions of most value (e.g. it will always move to the neighbouring square with highest value, if two or more squares have equal value they will have equal probability).
 
 # +
-vk=np.zeros((4,4))
+# Takes in a value array vk of size nRow*nCol
+# Returns a policy array of size nRow*nCol*4 (for up,down,left,right)
+# The policy return contains the probability that a particular action
+# will be followed
 def defineGreedyPolicy(vk):
     arrows='⇧⇩⇦⇨'
     nRow=vk.shape[0]
     nCol=vk.shape[1]
-    vl=[arrows]*16
-    vl=np.reshape(vl,(nRow,nCol))
-    for row in range(vl.shape[0]):
-        for col in range(vl.shape[1]):
+    pol=np.zeros((nRow,nCol,4))
+    for row in range(nRow):
+        for col in range(nCol):
             #Check if we are in terminal state
             if (row==0 and col==0) or (row==nRow-1 and col==nCol-1):
-                vl[row][col]=""
                 continue
             #Update our policy based on the current value of the cells nearby
-            dirs=[
-                vk[max(0,row-1)][col], #Up
-                vk[min(nRow-1,row+1)][col], #Down
-                vk[row][max(0,col-1)], #Left
-                vk[row][min(nCol-1,col+1)]] #Right
+            dirs=np.zeros(4)
+            for way in Dir:
+                dirs[way]=gw.get(vk,row,col,way)
                 
             maxVal=np.max(dirs)
             #print(row,col,dirs)
+            val=0
+            for i in Dir:
+                if dirs[i] >= maxVal-1e-9: #The 1e-9 is just for precision in larger grids
+                    pol[row][col][i]=1
+    
+    #Normalise our probabilities
+    for row in range(nRow):
+        for col in range(nCol):
+            norm=np.sum(pol[row][col])
+            pol[row][col]=np.divide(pol[row][col],norm,out=np.zeros_like(pol[row][col]),where=norm!=0)
+    
+    
+    return pol
+
+def convertPolicyToString(pol):
+    arrows='⇧⇩⇦⇨'
+    nRow=pol.shape[0]
+    nCol=pol.shape[1]
+    arStr=[arrows]*(nRow*nCol)
+    arStr=np.reshape(arStr,(nRow,nCol))
+    for row in range(nRow):
+        for col in range(nCol):
             val=''
-            for i in range(4):
-                if dirs[i] >= maxVal:
-                    val+=arrows[i]
-            vl[row][col]=val
-            
-    return vl
+            for way in Dir:
+                if(pol[row][col][way]>0):
+                    val+=arrows[way]
+            arStr[row][col]=val
+    return arStr
 
+
+vk=gw.getZeroGrid()
 policy=defineGreedyPolicy(vk)
-plotGridWorld(policy,showCorners=False,title="Intial Policy")
-vk=np.zeros((4,4))
+plotGridWorld(convertPolicyToString(policy),showCorners=False,title="Initial Policy")
 
-vk=updateVk(vk) #Do first update
-policy=defineGreedyPolicy(vk)
-plotGridWorld(policy,showCorners=False,title="Step 1")
-
-
-plotHere=[2,3,4,5,10,20,40,80,120,160,200]
-for i in range(2,201):
-    vk=updateVk(vk)
+plotHere=[1,2,3,4,5,10,20,40,80,120,160,200]
+for i in range(1,201):
+    vk=updateVk(vk,rp)
     policy=defineGreedyPolicy(vk)
     if i in plotHere:
-        plotGridWorld(policy,showCorners=False,title=str(i)+" steps")
+        if i == 1:
+            title="Greedy -- 1 step"
+        else:
+            title="Greedy -- "+str(i)+" steps"
+        plotGridWorld(convertPolicyToString(policy),showCorners=False,title=title)
 
 
 # -
+
+# ### How do we do even better?
+# We start with any policy, $\pi$, then:
+# 1. Use $\pi$ to evaluate the value, $V$
+# 2. Use the greedy algorithm to determine a better policy, $\hat{\pi}_{*}$, based on moving to the highest value $V$
+# 3. Rinse, repeat using $\pi=\hat{\pi}_{*}$,
+# 4. Stop when we reach some convergence (e.g. when the values or policy don't change)
+#
+# At this point we should have reached the optimal policy $\pi_*$
+
+# +
+vk=gw.getZeroGrid()
+policy=defineGreedyPolicy(vk)
+plotGridWorld(convertPolicyToString(policy),showCorners=False,title="Initial Policy")
+
+plotHere=[1,2,3,4,5,10,20,40,80,120,160,200]
+for i in range(1,6):
+    vk=updateVk(vk,policy)
+    policy=defineGreedyPolicy(vk)
+    if i in plotHere:
+        if i == 1:
+            title="$\hat{\pi}_*$ -- 1 step"
+        else:
+            title="$\hat{\pi}_*$ -- "+str(i)+" steps"
+        plotGridWorld(convertPolicyToString(policy),showCorners=False,title=title)
+
+
+# -
+
+# ### Have we reached the optimal policy??
+#
+# To check we have reached the optimal policy we can try and use the policy to evaluate the optimal value function. 
+
+vk=gw.getZeroGrid()
+pistar=policy
+for i in range(5):
+    vk=updateVk(vk,pistar)
+    plotGridWorld(vk,showCorners=True,title=r"$v_\pi$ -- "+str(i)+" steps",applyFormat=True)
+
+# ### Summary
+# The optimal policy achieves the optimal value after $N-1$ steps, where $N$ is the size of grid (well $N \times N$).
 
 
