2bdb8484ab8bb5839a4f9f0ec53701d07178ee31
Ryan Nichol <r.nichol@ucl.ac.uk>
2020-12-08 13:54:52 +0000

    Adding a loss plot

--- Week9/Week9_GAN.py
+++ Week9/Week9_GAN.py
@@ -215,6 +215,11 @@ num_examples_to_generate = 16
 # to visualize progress in the animated GIF)
 test_seed = tf.random.normal([num_examples_to_generate, NOISE_LENGTH])
 
+gen_loss_array=np.zeros(EPOCHS)
+disc_loss_array=np.zeros(EPOCHS)
+gen_loss_err=np.zeros(EPOCHS)
+disc_loss_err=np.zeros(EPOCHS)
+
 
 # -
 
@@ -225,63 +230,77 @@ def train_step(images):
     noise = tf.random.normal([BATCH_SIZE, NOISE_LENGTH])
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, training=True)
+        generated_images = generator(noise, training=True)
 
-      real_output = discriminator(images, training=True)
-      fake_output = discriminator(generated_images, training=True)
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
 
-      gen_loss = generator_loss(fake_output)
-      disc_loss = discriminator_loss(real_output, fake_output)
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
     gen_optimiser.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     dis_optimiser.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    return gen_loss,disc_loss
 
 
 def train(dataset, epochs):
-  for epoch in range(epochs):
-    start = time.time()
+    for epoch in range(epochs):
+        start = time.time()
+        gloss=0
+        glossSq=0
+        dloss=0
+        dlossSq=0
+        N=len(dataset)
+        for image_batch in dataset:
+            gl,dl=train_step(image_batch)
+            gloss+=gl
+            glossSq+=gl**2
+            dloss+=dloss
+            dlossSq+=dl**2
 
-    for image_batch in dataset:
-      train_step(image_batch)
+        gen_loss_array[epoch]=gloss/N
+        disc_loss_array[epoch]=dloss/N
+        gen_loss_err[epoch]=np.sqrt(glossSq-gloss**2)/N
+        disc_loss_err[epoch]=np.sqrt(dlossSq-dloss**2)/N
 
-    # Produce images for the GIF as we go
+        # Produce images for the GIF as we go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                 epoch + 1,
+                                 test_seed)
+
+        # Save the model every 15 epochs
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
     display.clear_output(wait=True)
     generate_and_save_images(generator,
-                             epoch + 1,
-                             test_seed)
-
-    # Save the model every 15 epochs
-    if (epoch + 1) % 15 == 0:
-      checkpoint.save(file_prefix = checkpoint_prefix)
-
-    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-
-  # Generate after the final epoch
-  display.clear_output(wait=True)
-  generate_and_save_images(generator,
-                           epochs,
-                           test_seed)
+                            epochs,
+                            test_seed)
 
 
 # #### Generate and save images to produce an animated gif
 
 def generate_and_save_images(model, epoch, test_input):
-  # Notice `training` is set to False.
-  # This is so all layers run in inference mode (batchnorm).
-  predictions = model(test_input, training=False)
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
 
-  fig = plt.figure(figsize=(4,4))
+    fig = plt.figure(figsize=(4,4))
 
-  for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
-      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-      plt.axis('off')
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
 
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
 
 
 # # Step 8: Do the training
@@ -302,21 +321,29 @@ checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 anim_file = 'dcgan.gif'
 
 with imageio.get_writer(anim_file, mode='I') as writer:
-  filenames = glob.glob('image*.png')
-  filenames = sorted(filenames)
-  for filename in filenames:
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
     image = imageio.imread(filename)
     writer.append_data(image)
-  image = imageio.imread(filename)
-  writer.append_data(image)
 # -
 
 # # Step 10: Look at the gif
 # There are ways to display the GIF from the python code, or you can just do it using Markdown like I have done here.
 # ![Numbers](dcgan.gif "numbers")
 
+# +
 #Here is an example of the same thing but from the python cell using IPython
-from IPython.display import Image
-Image(filename="dcgan.gif")
+#from IPython.display import Image
+#Image(filename="dcgan.gif")
+# -
 
-
+fig,ax=plt.subplots()
+epochs=np.arange(EPOCHS)
+ax.plot(epochs,gen_loss_array,dy=gen_loss_err,label="Generator Loss")
+ax.plot(epochs,disc_loss_array,dy=disc_loss_err,label="Discriminator Loss")
+ax.set_xlabel("Training Epoch")
+ax.set_ylabel("Loss")
+ax.legend()
